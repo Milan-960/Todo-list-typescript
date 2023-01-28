5b157a9a1f888e5dd0e64bc80b12a6d7d1664e94
milan sachani <milan.sachani.ms@gmail.com>
2023-01-28 13:05:51 +0100

    partials added

--- src/Particles/Particles.tsx
+++ src/Particles/Particles.tsx
@@ -15,51 +15,67 @@ const TsParticles = () => {
     <Particles
       init={particlesInit}
       options={{
-        background: {
-          color: {
-            value: "#d8e6fc",
-          },
-        },
-        fpsLimit: 10,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 110,
-              duration: 0.3,
-            },
-          },
+        fullScreen: {
+          enable: true,
+          zIndex: 0,
         },
         particles: {
+          number: {
+            value: 200,
+            limit: 300,
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+          },
           color: {
             value: "#ffffff",
           },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000",
+            },
+            polygon: {
+              nb_sides: 5,
+            },
+            image: {
+              src: "images/github.svg",
+              width: 100,
+              height: 100,
+            },
           },
-          collisions: {
+          opacity: {
+            value: 0.5,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.5,
+              sync: false,
+            },
+          },
+          size: {
+            value: 30,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 10,
+              size_min: 10,
+              sync: false,
+            },
+          },
+          line_linked: {
             enable: true,
+            distance: 100,
+            color: "#ffffff",
+            opacity: 1,
+            width: 1,
           },
           move: {
             enable: true,
-            speed: 1,
+            speed: 3,
             direction: "none",
             random: false,
             straight: false,
@@ -71,38 +87,58 @@ const TsParticles = () => {
               rotateY: 1200,
             },
           },
-          number: {
-            value: 20,
-            density: {
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onHover: {
               enable: true,
-              value_area: 800,
+              mode: "bubble",
+              parallax: {
+                enable: false,
+                force: 60,
+                smooth: 10,
+              },
             },
-          },
-          opacity: {
-            value: 2,
-            random: true,
-            anim: {
+            onClick: {
               enable: true,
-              speed: 1,
-              opacity_min: 0.1,
-              sync: false,
+              mode: "push",
             },
+            resize: true,
           },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: 5,
-            random: true,
-            anim: {
-              enable: true,
+          modes: {
+            grab: {
+              distance: 400,
+              lineLinked: {
+                opacity: 1,
+              },
+            },
+            bubble: {
+              distance: 400,
+              size: 100,
+              duration: 2,
+              opacity: 1,
               speed: 2,
-              size_min: 3,
-              sync: false,
+            },
+            repulse: {
+              distance: 200,
+            },
+            push: {
+              particles_nb: 4,
+            },
+            remove: {
+              particles_nb: 2,
             },
           },
         },
-        detectRetina: true,
+        backgroundMask: {
+          enable: true,
+        },
+        retina_detect: true,
+        fps_limit: 60,
+        background: {
+          image: "url('https://particles.js.org/images/background3.jpg')",
+        },
       }}
     />
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import TsParticles from "./Particles/Particles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <TsParticles />
     <App />
   </React.StrictMode>
 );
